5e12cedb6ca7a9fff2c1c6fd3c5e199a4870be1b
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-04 21:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+window.onload = function() {
+  // Get the div element
+  var div = document.getElementById('browser-info');
+
+  // Get the browser name and version
+  var browserName = navigator.appName;
+  var version = navigator.appVersion;
+
+  // Create the message
+  var message = "You are using " + browserName + " version " + version;
+
+  // Add the message to the div
+  div.innerHTML = message;
+}
