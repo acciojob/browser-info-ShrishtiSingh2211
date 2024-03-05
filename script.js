2cb0172dd292d3e7601c6fa785d10071578e05eb
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-05 22:13:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ window.onload = function() {
   // Get the div element
   var div = document.getElementById('browser-info');
 
-  // Get the browser name and version
-  var browserName = navigator.appName;
-  var version = navigator.appVersion;
+  // Get the user agent string
+  var userAgent = navigator.userAgent;
+
+  // Initialize browserName with an empty string
+  var browserName = "";
+
+  // Check if user agent string contains the name of one of the browsers
+  if (userAgent.indexOf("Chrome") > -1) {
+    browserName = "Chrome";
+  } else if (userAgent.indexOf("Safari") > -1) {
+    browserName = "Safari";
+  } else if (userAgent.indexOf("Opera") > -1) {
+    browserName = "Opera";
+  } else if (userAgent.indexOf("Firefox") > -1) {
+    browserName = "Firefox";
+  } else if (userAgent.indexOf("MSIE") > -1) {
+    browserName = "Internet Explorer";
+  }
 
   // Create the message
-  var message = "You are using " + browserName + " version " + version;
+  var message = "You are using " + browserName;
 
   // Add the message to the div
   div.innerHTML = message;
